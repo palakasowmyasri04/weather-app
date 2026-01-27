7c40d803a19e65c8a079acfa671f62cda1046c26
Palaka Sowmya Sri <sowmyasri33833@gmail.com>
2026-01-27 19:43:37 +0530

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -2,8 +2,9 @@ import requests
 from datetime import datetime
 from config import API_KEY, BASE_URL
 
+def main():
+    city = input("Enter city name: ")
 
-def get_weather(city):
     params = {
         "q": city,
         "appid": API_KEY,
@@ -14,35 +15,15 @@ def get_weather(city):
 
     if response.status_code == 200:
         data = response.json()
-        return {
-            "city": data["name"],
-            "temperature": data["main"]["temp"],
-            "condition": data["weather"][0]["description"],
-            "humidity": data["main"]["humidity"]
-        }
+        now = datetime.now().strftime("%d %b %Y %H:%M")
+
+        print("\nCity:", data["name"])
+        print("Date & Time:", now)
+        print("Temperature:", data["main"]["temp"], "°C")
+        print("Condition:", data["weather"][0]["description"])
+        print("Humidity:", data["main"]["humidity"], "%")
     else:
-        return None
-
-
-def main():
-    city = input("Enter city name: ")
-
-    weather = get_weather(city)
-
-    if weather:
-        current_time = datetime.now().strftime("%d %b %Y, %I:%M %p")
-
-        print("\n🌍 Weather Report")
-        print("-" * 25)
-        print(f"📍 City       : {weather['city']}")
-        print(f"📅 Date & Time: {current_time}")
-        print(f"🌡 Temperature: {weather['temperature']}°C")
-        print(f"☁ Condition  : {weather['condition'].title()}")
-        print(f"💧 Humidity   : {weather['humidity']}%")
-
-    else:
-        print("❌ City not found or API error.")
-
+        print("City not found or API error.")
 
 if __name__ == "__main__":
     main()
